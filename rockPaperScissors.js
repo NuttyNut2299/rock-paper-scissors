538f0520a89eab5c594a680ef2667f7a512c545f
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-16 15:17:32 +0800

    Add new button for resetting game, Add all button click events in rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,21 +6,21 @@ function getComputerChoice () {
     return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice (choice) {
-    // let choice = prompt("Pick: Rock, Paper, Scissors");
+// function getHumanChoice (choice) {
+//     let choice = prompt("Pick: Rock, Paper, Scissors");
 
-    // if (choice.toLowerCase() === "rock") {
-    //     return "rock";
-    // } else if (choice.toLowerCase() === "paper") {
-    //     return "paper";
-    // } else if (choice.toLowerCase() === "scissors") {
-    //     return "scissors";
-    // } else {
-    //     return "scissors"; // default to scissors 
-    // }
+//     if (choice.toLowerCase() === "rock") {
+//         return "rock";
+//     } else if (choice.toLowerCase() === "paper") {
+//         return "paper";
+//     } else if (choice.toLowerCase() === "scissors") {
+//         return "scissors";
+//     } else {
+//         return "scissors"; // default to scissors 
+//     }
 
-    return choice;
-}
+//     return choice;
+// }
 
 // function returnTie () {
 //     console.log("Tie!");
@@ -37,45 +37,54 @@ function getHumanChoice (choice) {
 //     return "computer";
 // }
 
-function playRound (humanChoice, computerChoice, roundNumber) {
+function playRound (humanChoice, computerChoice) {
     let result = "";
 
-    console.log("-- Round " + roundNumber + " START! --");
+    // console.log("-- Round " + roundNumber + " START! --");
 
     if (humanChoice.toLowerCase() === "rock") {
-        console.log("Human picked Rock!");
-        if (computerChoice == 0) {
+        // console.log("Human picked Rock!");
+        if (computerChoice === 0) {
             // console.log("Computer picked Rock!");
+            choiceDisplay.textContent = "Human picked Rock! Computer picked Rock! Tie!";
             result = "tie";
-        } else if (computerChoice == 1) {
+        } else if (computerChoice === 1) {
             // console.log("Computer picked Paper!");
+            choiceDisplay.textContent = "Human picked Rock! Computer picked Paper! Computer win!";
             result = "computer";
         } else {
             // console.log("Computer picked Scissors!");
+            choiceDisplay.textContent = "Human picked Rock! Computer picked Scissors! Human win!";
             result = "human";
         }
     } else if (humanChoice.toLowerCase() === "paper") {
-        console.log("Human picked Paper!");
-        if (computerChoice == 0) {
+        // console.log("Human picked Paper!");
+        if (computerChoice === 0) {
             // console.log("Computer picked Rock!");
+            choiceDisplay.textContent = "Human picked Paper! Computer picked Rock! Human win!";
             result = "human";
-        } else if (computerChoice == 1) {
+        } else if (computerChoice === 1) {
             // console.log("Computer picked Paper!");
+            choiceDisplay.textContent = "Human picked Paper! Computer picked Paper! Tie!";
             result = "tie";
         } else {
             // console.log("Computer picked Scissors!");
+            choiceDisplay.textContent = "Human picked Paper! Computer picked Scissors! Computer win!";
             result = "computer";
         }
     } else {
-        console.log("Human picked Scissors!");
-        if (computerChoice == 0) {
+        // console.log("Human picked Scissors!");
+        if (computerChoice === 0) {
             // console.log("Computer picked Rock!");
+            choiceDisplay.textContent = "Human picked Scissors! Computer picked Rock! Computer win!";
             result = "computer";
-        } else if (computerChoice == 1) {
+        } else if (computerChoice === 1) {
             // console.log("Computer picked Paper!");
+            choiceDisplay.textContent = "Human picked Scissors! Computer picked Paper! Human win!";
             result = "human";
         } else {
             // console.log("Computer picked Scissors!");
+            choiceDisplay.textContent = "Human picked Scissors! Computer picked Scissors! Tie!";
             result = "tie";
         }
     }
@@ -116,6 +125,46 @@ function playRound (humanChoice, computerChoice, roundNumber) {
 //     return "Until next time!";
 // }
 
+function playGame(result) {
+    if (result === "human") {
+        humanScore+=1;
+    } else if (result === "computer") {
+        computerScore+=1;
+    } else {
+        // do nothing
+    }
+    scoreDisplay.textContent = "Human Score: " + humanScore + "! Computer Score: " + computerScore + "!";
+
+    if (humanScore === 5) {
+        winnerDisplay.textContent = "Human is the champion for winning 5 rounds!"
+        disableButtons();
+    } else if (computerScore === 5) {
+        winnerDisplay.textContent = "Computer is the champion for winning 5 rounds!"
+        disableButtons();
+    } else {
+        // do nothing
+    }
+}
+
+function disableButtons() {
+    btnRock.disabled = true;
+    btnPaper.disabled = true;
+    btnScissors.disabled = true;
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    btnRock.disabled = false;
+    btnPaper.disabled = false;
+    btnScissors.disabled = false;
+
+    choiceDisplay.textContent = "";
+    scoreDisplay.textContent = "";
+    winnerDisplay.textContent = "";
+}
+
 // console.log("The computer picked " + getComputerChoice() + "!");
 
 // console.log(getHumanChoice());
@@ -125,6 +174,11 @@ function playRound (humanChoice, computerChoice, roundNumber) {
 // console.log(playGame());
 
 const container = document.querySelector(".container");
+
+const rules = document.createElement("h1");
+rules.textContent = "Pick your choice! First to 5 wins";
+
+container.appendChild(rules);
 
 const btnRock = document.createElement("button");
 const btnPaper = document.createElement("button");
@@ -141,3 +195,38 @@ container.appendChild(btnScissors);
 btnRock.setAttribute("style", "width: 200px; height: 50px; margin: 12px; font-size: 24px;");
 btnPaper.setAttribute("style", "width: 200px; height: 50px; margin: 12px; font-size: 24px;");
 btnScissors.setAttribute("style", "width: 200px; height: 50px; margin: 12px; font-size: 24px;");
+
+const choiceDisplay = document.createElement("div");
+const scoreDisplay = document.createElement("div");
+const winnerDisplay = document.createElement("div");
+
+scoreDisplay.textContent = "Human Score: " + humanScore + "! Computer Score: " + computerScore + "!";
+
+container.appendChild(choiceDisplay);
+container.appendChild(scoreDisplay);
+container.appendChild(winnerDisplay);
+
+const btnReset = document.createElement("button");
+
+btnReset.textContent = "Reset Game";
+
+container.appendChild(btnReset);
+
+btnRock.addEventListener("click", () => {
+    const result = playRound(btnRock.textContent, getComputerChoice());
+    playGame(result);
+});
+
+btnPaper.addEventListener("click", () => {
+    const result = playRound(btnPaper.textContent, getComputerChoice());
+    playGame(result);
+});
+
+btnScissors.addEventListener("click", () => {
+    const result = playRound(btnScissors.textContent, getComputerChoice());
+    playGame(result);
+});
+
+btnReset.addEventListener("click", () => {
+    resetGame();
+});
